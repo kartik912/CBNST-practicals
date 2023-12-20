4a2dc6cf7bc44a0f9ed2823e810cecf6ff971d44
kartik912 <kartik912@github.com>
2023-12-20 11:13:24 +0530

    Adding the codes

--- P9_Gauss_seidal.c
+++ P9_Gauss_seidal.c
@@ -24,22 +24,23 @@ main()
         {
             s=0;
             for(j=0;j<X;j++)
-            if(j!=i) s+=x[i][j]*a[j];
-            t=(x[i][X]-s)/x[i][i];
-            e=fabs(a[i]-t);
-            a[i]=t;
+                if(j!=i)  {
+                    s+=x[i][j]*a[j];
+                }
+                t=(x[i][X]-s)/x[i][i];
+                e=fabs(a[i]-t);
+                a[i]=t;
         }
         printf(" %5d\t",r);
         for(i=0;i<X;i++)
-        printf(" %9.4f\t",a[i]);
+            printf(" %9.4f\t",a[i]);
         printf("\n");
         if(max<ae)
         {
             printf(" Converses in %3d iteration\n", r);
             for(i=0;i<X;i++)
-            printf("a[%3d]=%7.4f\n", i+1,a[i]);
+                printf("a[%3d]=%7.4f\n", i+1,a[i]);
             return 0;
         }
-
-        }
     }
+}
